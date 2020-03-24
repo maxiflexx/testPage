63cd793462dc643382f92a74100fa28defceaf11
Keunhyeok Lim <38815618+limkeunhyeok@users.noreply.github.com>
2020-03-24 20:13:43 +0900

    express-ejs-layouts 적용

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', [
+    path.join(__dirname, '/views/pages'),
+    path.join(__dirname, 'views/partials')
+])
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:false}));
+app.set('layout', 'layout');
+app.use(expressLayouts);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
     let drinks = [
         { name: 'Bloody Mary', drunkness: 3 },
         { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
+        { name: 'Scotch', drunkness: 10 },
     ];
     let tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
-    res.render('pages/index', {
+    res.render('index.ejs', {
         drinks: drinks,
-        tagline: tagline
+        tagline: tagline,
+        title: 'mainPage'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.render('pages/about');
+    res.render('about.ejs', {title: 'aboutPage'});
 });
 
 app.listen(3000, () => {
