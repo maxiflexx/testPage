2f37c23a3888fa8b4902c2ef6823e99e30966cd0
Keunhyeok Lim <38815618+limkeunhyeok@users.noreply.github.com>
2020-03-25 21:06:41 +0900

    부트스트랩 적용

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.set('view engine', 'ejs');
 app.set('views', [
     path.join(__dirname, '/views/pages'),
@@ -20,6 +24,24 @@ app.get('/', (req, res) => {
     let drinks = [
         { name: 'Bloody Mary', drunkness: 3 },
         { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
+        { name: 'Scotch', drunkness: 10 },
         { name: 'Scotch', drunkness: 10 },
     ];
     let tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
