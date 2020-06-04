da457e4a0738c81af546305bf2dd97c0c214798e
Keunhyeok Lim <38815618+limkeunhyeok@users.noreply.github.com>
2020-06-04 17:23:38 +0900

    index 페이지 디자인 완료

--- app.js
+++ app.js
@@ -61,16 +61,7 @@ app.get('*', (req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    let drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 },
-    ];
-    let tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
     res.render('index.ejs', {
-        drinks: drinks,
-        tagline: tagline,
         title: 'mainPage' 
     });
 });
