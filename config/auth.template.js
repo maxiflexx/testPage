933288f6209726afc4e44c6b0edd1a0e9b8a39f5
Keunhyeok Lim <38815618+limkeunhyeok@users.noreply.github.com>
2020-07-25 02:13:40 +0900

    register mysql 연동 완료

---
+++ config/auth.template.js
@@ -0,0 +1,11 @@
+module.exports = {
+    gmail: {
+        id: "",
+        pwd: ""
+    },
+    auth: {
+        status: false,
+        email: "",
+        code: ""
+    }
+}
--- controller/authController.js
+++ controller/authController.js
@@ -2,62 +2,67 @@ const db = require('../db/db');
 const validator = require('../lib/validator');
 const nodemailer = require('nodemailer');
 const authConfig = require('../config/auth');
+const oatuh = require('../config/oauth.json');
+const bcrypt = require('bcrypt');
 
 class AuthController {
     register (req, res, next) {
-        if (!(validator.email(req.body.email))) {
+        if (!(authConfig.auth.status)) {
             res.send(`
                 <script type="text/javascript">
-                    alert("Invalid email");
+                    alert("인증 코드를 작성하세요.");
                     window.location.href = "/register";
                 </script>`);
-        } else if (!(validator.password(req.body.pwd, req.body.re_pwd))) {
-            res.send(`
-                <script type="text/javascript">
-                    alert("Invalid password");
-                    window.location.href = "/register";
-                </script>`);
-        } else if (!(validator.phoneNumber(req.body.phone_number))) {
-            res.send(`
-                <script type="text/javascript">
-                    alert("Invalid phone number");
-                    window.location.href = "/register";
-                </script>`);
+            return;
         } else {
-            db.query('SELECT * FROM user WHERE email = (?)', [req.body.email], function (err, results, fields) {
-                if (err) {
-                    err.message = 'DB Operation Failure';
-                        err.status = 500;
-                        console.log(err)
-                        next(err);
-                        return;
-                };
-                if (results.length !== 0) {
-                    res.send(`
-                        <script type="text/javascript">
-                            alert("Email duplicates");
-                            window.location.href = "/register";
-                        </script>`);
-                    return;
-                } else {
-                    db.query('SELECT * FROM user WHERE phonenumber = (?)', [req.body.phone_number], function (err, results, fields) {
-                        if (err) {
-                            err.message = 'DB Operation Failure';
-                                err.status = 500;
-                                console.log(err)
-                                next(err);
-                                return;
-                        };
-                        if (results.length !== 0) {
-                            res.send(`
-                                <script type="text/javascript">
-                                    alert("Phone number duplicates");
-                                    window.location.href = "/register";
-                                </script>`);
+            if (authConfig.auth.email !== req.body.email) {
+                res.send(`
+                    <script type="text/javascript">
+                        alert("메일을 바르게 입력하세요.");
+                        window.location.href = "/register";
+                    </script>`);
+                return;
+            } else if (authConfig.auth.code !== req.body.code) {
+                res.send(`
+                    <script type="text/javascript">
+                        alert("코드를 바르게 입력하세요.");
+                        window.location.href = "/register";
+                    </script>`);
+                return;
+            } else if (!(validator.password(req.body.pwd, req.body.re_pwd))) {
+                res.send(`
+                    <script type="text/javascript">
+                        alert("패스워드를 바르게 입력하세요.");
+                        window.location.href = "/register";
+                    </script>`);
+                return;
+            } else if (!(validator.phoneNumber(req.body.phone_number))) {
+                res.send(`
+                    <script type="text/javascript">
+                        alert("핸드폰 번호를 바르게 입력하세요.");
+                        window.location.href = "/register";
+                    </script>`);
+                return;
+            } else {
+                db.query('SELECT * FROM user WHERE phonenumber = (?)', [req.body.phone_number], function (err, results, fields) {
+                    if (err) {
+                        err.message = 'DB Operation Failure';
+                            err.status = 500;
+                            console.log(err)
+                            next(err);
                             return;
-                        } else {
+                    };
+                    if (results.length !== 0) {
+                        res.send(`
+                            <script type="text/javascript">
+                                alert("핸드폰 번호가 중복됩니다.");
+                                window.location.href = "/register";
+                            </script>`);
+                        return;
+                    } else {
+                        bcrypt.hash(req.body.pwd, 10, (err, hash) => {
                             db.query('INSERT INTO user (email, password, username, phonenumber) VALUES (?, ?, ?, ?)',
-                                [req.body.email, req.body.pwd, req.body.username, req.body.phone_number],
+                                [req.body.email, hash, req.body.username, req.body.phone_number],
                                 function (err, results, fields) {
                                     if (err) {
                                         err.message = 'DB Operation Failure';
@@ -67,56 +72,79 @@ class AuthController {
                                         return;
                                     };
                                     console.log(req.body.email);
-                                    res.redirect(301, '/');
-                                    return;
-                                });
-                        };
-                    });
-                };
-            });
+                                    return res.redirect(301, '/');
+                            });
+                        })
+                        
+                    };
+                });
+            };
         };
     };
 
     mailSender (req, res, next) {
-        if (authConfig.auth.status === true) {
-            res.send(false);
-            return;
-        } else {
-            authConfig.auth.status = true;
-            authConfig.auth.code = req.body.code;
-            let transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: authConfig.gmail.id,
-                    pass: authConfig.gmail.pwd
-                }
-            });
-    
-            let mailOptions = {
-                from: authConfig.gmail.id,
-                to: req.body.email,
-                subject: 'Mailer Test',
-                html: `
-                    <p>Please click the link below.</p>
-                    <p>${authConfig.auth.code}</p>   
-                `
-            };
-    
-            transporter.sendMail(mailOptions, (err, info) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
-
-
-            console.log(req.body)
-            res.send(true);
+        if (!(validator.email(req.body.email))) {
+            res.send({ status: false, message: '메일을 바르게 입력하세요.'});
             return;
         };
-        
-        
+
+        if (authConfig.auth.status) {
+            res.send({ status: false, message: '이미 발송했습니다.'});
+            return;
+        } else {
+            db.query('SELECT * FROM user WHERE email=(?)', [req.body.email], function (err, results, fields) {
+                if (err) {
+                    err.message = 'DB Operation Failure';
+                    err.status = 500;
+                    console.log(err)
+                    next(err);
+                }
+                if (results.length !== 0) {
+                    res.send({ status: false, message: '이전에 가입한 적이 있습니다.'});
+                    return;
+                } else {
+                    // https://blog.eunsatio.io/develop/nodemailer%EC%99%80-gmail%EB%A1%9C-%EB%A9%94%EC%9D%BC-%EB%B0%9C%EC%86%A1%ED%95%98%EA%B8%B0-%E3%85%A1-OAuth2 참고
+                    authConfig.auth.code = req.body.code;
+                    let transporter = nodemailer.createTransport({
+                        host: 'smtp.gmail.com',
+                        port: 465,
+                        secure: true,
+                        auth: {
+                            type: 'OAuth2',
+                            user: authConfig.gmail.id,
+                            clientId: oatuh.web.client_id,
+                            clientSecret: oatuh.web.client_secret,
+                            refreshToken: oatuh.web.refresh_token,
+                            accessToken: oatuh.web.access_token,
+                            expires: 3600
+                        }
+                    });
+                    let mailOptions = {
+                        from: authConfig.gmail.id,
+                        to: req.body.email,
+                        subject: 'Mailer Test',
+                        html: `
+                            <p>Please click the link below.</p>
+                            <p>${authConfig.auth.code}</p>   
+                        `
+                    };
+                    transporter.sendMail(mailOptions, (err, info) => {
+                        if (err) {
+                            console.log(err);
+                            res.send({ status: false, message: '발송 실패!! 다시 시도해주세요.'});
+                            return;
+                        } else {
+                            console.log('Email sent: ' + info.response);
+                            authConfig.auth.status = true;
+                            authConfig.auth.email = req.body.email;
+                            res.send({ status: true, message: '메일을 발송했습니다. 코드를 입력해주세요.'});
+                            return;
+                        };
+                    });
+                    transporter.close();
+                };
+            });
+        };
     };
 };
 
